dbf238e36ae91a3f388f11fc107537d13098c476
Kissmybullet <128668418+Kissmybullet@users.noreply.github.com>
2024-02-21 23:14:58 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,14 +38,14 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamAlexa/AlexaMusic",
+    "https://github.com/Kissmybullet/AlexaMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alexa_BotUpdates")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Alexa_Help")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Entertainment_AG")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/kdrabbd")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
